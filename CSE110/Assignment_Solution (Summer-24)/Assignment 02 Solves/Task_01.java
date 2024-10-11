010e47df9b223437f2393fbef5095f582cd4c7a5
Md. Farden Alam Sun <sun26312@gmail.com>
2024-10-11 21:19:28 +0600

    Create Task_01.java
    Signed-off-by: Md. Farden Alam Sun <sun26312@gmail.com>

---
+++ CSE110/Assignment_Solution (Summer-24)/Assignment 02 Solves/Task_01.java
@@ -0,0 +1,18 @@
+
+class one {
+	public static void main(String args[]){
+		int hunger =1;  //valid
+		int 2AB=2;  //not valid
+		int 312.2=3;  //not valid
+		int MOBILE=4;  //valid
+		int "Ans"=5;  //not valid
+		int $30=6;  //valid
+		int Yes/No=7;  //not valid
+		int Student-id=8;  //not valid
+		int A+3=9;  //not valid
+		int X=10;  //valid
+		int return =11;  //not valid
+		
+	}
+}
+
