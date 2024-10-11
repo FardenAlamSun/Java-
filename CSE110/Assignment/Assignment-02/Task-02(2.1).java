3d43de3f025468e7e7e1c869b7f2e92c55bd6bf0
Md. Farden Alam Sun <sun26312@gmail.com>
2024-10-11 21:08:24 +0600

    Delete CSE110/Assignment directory
    Signed-off-by: Md. Farden Alam Sun <sun26312@gmail.com>

--- CSE110/Assignment/Assignment-02/Task-02(2.1).java
+++
@@ -1,7 +0,0 @@
-class one {
-	public static void main(String args[]) {
-		int x = 10;
-		System.out.print(x);
-		
-	}
-}
--- CSE110/Assignment/Assignment-02/Task02(2.2).java
+++
@@ -1,8 +0,0 @@
-class one {
-	public static void main(String args[]) {
-		int x = 10;
-		int y = 11;
-		int sum = x + y;
-		System.out.print(sum);
-	}
-}
