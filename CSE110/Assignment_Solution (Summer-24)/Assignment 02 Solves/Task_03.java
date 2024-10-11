e67434a980f86a7d525c279c0cc665ba31a37337
Md. Farden Alam Sun <sun26312@gmail.com>
2024-10-11 21:15:53 +0600

    Create Task_03.java
    Signed-off-by: Md. Farden Alam Sun <sun26312@gmail.com>

---
+++ CSE110/Assignment_Solution (Summer-24)/Assignment 02 Solves/Task_03.java
@@ -0,0 +1,10 @@
+class one {
+	public static void main(String args[]) {
+		float pi = 3.1416f;
+		int r = 4;
+		float C = 2 * pi * r;
+		float A = pi * r * r;
+		System.out.println("The circumference is " + C);
+		System.out.print("Area of a circle is " + A);
+	}
+}
