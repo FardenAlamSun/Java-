5b18a1a00510f2cbc306423bfd3b3c63cf771eec
Md. Farden Alam Sun <sun26312@gmail.com>
2024-10-11 21:22:48 +0600

    Create Task_12.java
    Signed-off-by: Md. Farden Alam Sun <sun26312@gmail.com>

---
+++ CSE110/Assignment_Solution (Summer-24)/Assignment 02 Solves/Task_12.java
@@ -0,0 +1,13 @@
+class one {
+	public static void main(String args[]) {
+		int n = 23221454;
+		int a = n % 100;
+		int b = a / 10;
+		int c = a % 10;
+		System.out.println(c);
+		System.out.println(b);
+		
+	}
+	
+}
+	
