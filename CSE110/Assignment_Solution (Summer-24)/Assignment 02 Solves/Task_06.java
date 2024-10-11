affe29a488392f80fd4b52f5be48dc20c8b5e737
Md. Farden Alam Sun <sun26312@gmail.com>
2024-10-11 21:18:55 +0600

    Create Task_06.java
    Signed-off-by: Md. Farden Alam Sun <sun26312@gmail.com>

---
+++ CSE110/Assignment_Solution (Summer-24)/Assignment 02 Solves/Task_06.java
@@ -0,0 +1,29 @@
+//Task___6(a)====>
+
+class one {
+	public static void main(String args[]) {
+		int x = 1;
+		int y = 2;
+		int t;
+		t = x;
+		x = y;
+		y = t;
+		System.out.print("Now : " + "x= " + x + " and y= " + y);
+	}
+	
+}
+
+
+
+//Task__6(b)=====>
+
+class one {
+	public static void main(String args[]) {
+		int x = 1;
+		int y = 2;
+		x = x + y;
+		y = x - y;
+		x = x - y;
+		System.out.print("Now : " + "x= " + x + " and y= " + y);
+	}
+	
