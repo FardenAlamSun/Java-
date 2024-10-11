d1fb375b3d47bacc469af362046a9deb509b9d64
Md. Farden Alam Sun <sun26312@gmail.com>
2024-10-11 21:19:59 +0600

    Create Task_02.java
    Signed-off-by: Md. Farden Alam Sun <sun26312@gmail.com>

---
+++ CSE110/Assignment_Solution (Summer-24)/Assignment 02 Solves/Task_02.java
@@ -0,0 +1,72 @@
+//2.1==
+
+class one {
+	public static void main(String args[]) {
+		int x = 10;
+		System.out.print(x);
+		
+	}
+}
+
+
+//2.2==
+
+class one {
+	public static void main(String args[]) {
+		int x = 10;
+		int y = 11;
+		int sum = x + y;
+		System.out.print(sum);
+	}
+}
+
+
+// 2.3==
+
+class one {
+	public static void main(String args[]) {
+		int x = 1;
+		int y = 2;
+		int z = x + y;
+		int zz = x / y;
+		System.out.print("The product is " + z + " and the division is " + zz);
+	}
+}
+
+
+// 2.4==
+
+class one {
+	public static void main(String args[]) {
+		double x = 1;
+		double y = 2;
+		double z = x + y;
+		double zz = x / y;
+		System.out.print("The product is " + z + " and the division is " + zz);
+	}
+}
+
+
+// 2.5==
+
+class one {
+	public static void main(String args[]) {
+		int x = 1;
+		double y = 2;
+		double z = x + y;
+		double zz = x / y;
+		System.out.print("The product is " + z + " and the division is " + zz);
+	}
+}
+
+
+// 2.6==
+
+class one {
+	public static void main(String args[]) {
+		int x = 1;
+		String y = "2";
+		String z = x + y; 
+		System.out.print(z);
+	}
+}
