2b9d949dcfff690d8f0ffee7be170beee5d3fcdc
Md. Farden Alam Sun <sun26312@gmail.com>
2024-10-11 21:17:22 +0600

    Create Task_05.java
    Signed-off-by: Md. Farden Alam Sun <sun26312@gmail.com>

---
+++ CSE110/Assignment_Solution (Summer-24)/Assignment 02 Solves/Task_05.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class one {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Given a value for inch: ");
+		int n = sc.nextInt();
+		double m = n * 0.0254;
+		System.out.print(n + " inch is " + m + " meters");
+		
+	}
+	
+}
