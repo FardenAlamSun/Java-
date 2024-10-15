350ea4a5361fd83b4433006614c9fa86ab8f4db0
Md. Farden Alam Sun <sun26312@gmail.com>
2024-10-15 21:53:11 +0600

    Create Test_01.java
    Signed-off-by: Md. Farden Alam Sun <sun26312@gmail.com>

---
+++ CSE110/Assignment_Solution (Summer-24)/Assignment 04 Solves/Test_01.java
@@ -0,0 +1,40 @@
+//Task--01==================================================>
+
+//(a)======================>
+
+class one {
+    public static void main(String args[]) {
+        int a = 24;
+        while (a >= -6) {
+            if (a == -6) {
+                System.out.print(a);
+            } else {
+                System.out.print(a + ", ");
+            }
+            a -= 6;
+        }
+
+    }
+}
+
+
+
+
+
+
+
+//(b)==================>
+
+class One {
+    public static void main(String[] args) {
+        int a = -10;
+        while (a <= 20) {
+            if (a == 20) {
+                System.out.print(a);
+            } else {
+                System.out.print(a + ",");
+            }
+            a += 5;
+        }
+    }
+}
